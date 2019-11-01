42d6bad102650cca4c24de3a76c2affce8ea0dd7
lucas <lucas.san.77@gmail.com>
2019-11-01 12:45:23 -0300

    chore: altera retorno da rota

--- exemplo_node/index.js
+++ exemplo_node/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/teste', (req,res) =>{
-    return res.json({hello: 'World'});
+    return res.json({hello: 'World 2'});
 });
 
 app.listen(3333);
