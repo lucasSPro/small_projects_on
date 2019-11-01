8fb33c93f7c52f9ac3d4adf54007b4611b644e84
lucas <lucas.san.77@gmail.com>
2019-11-01 14:45:51 -0300

    altera o valor 0 para 4 no retorno da rota

--- exemplo_node/index.js
+++ exemplo_node/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/teste', (req,res) =>{
-    return res.json({hello: 'World 3'});
+    return res.json({hello: 'World 4'});
 });
 
 app.listen(3333);
