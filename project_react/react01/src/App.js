e8cfd6d57747aab67c18c99ebf7c3dbd97198e02
lucas <lucas.san.77@gmail.com>
2019-11-06 23:24:51 -0300

    add simple route -React01

--- project_react/react01/src/App.js
+++ project_react/react01/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
-    );
-}
+import Routes from './routes';
 
+function App() {
+    return <Routes />
+}
 export default App;
