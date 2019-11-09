bdb53ef35af9d1d9a4bd5cd6d9bf02e6be337549
lucas <lucas.san.77@gmail.com>
2019-11-09 02:13:38 -0300

    add style and issueList -React01

--- project_react/react01/src/pages/Repository/styles.js
+++ project_react/react01/src/pages/Repository/styles.js
@@ -43,3 +43,59 @@ export const Owner = styled.header`
     }
 `;
 
+export const IssueList = styled.ul`
+    padding-top: 30px;
+    margin-top:30px;
+    border-top: 1px solid #265496;
+    list-style: none;
+
+    li {
+        display: flex;
+        padding: 15px 10px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+    }
+
+    & + li {
+        margin-top: 10px;
+    }
+
+    img {
+        width: 42px;
+        height:42px;
+        border-radius: 50%;
+        border: 2px solid #eee;
+    }
+
+    div {
+        flex: 1;
+        margin-left: 15px;
+
+        strong {
+            font-size: 16px;
+
+            span {
+                color: #fff;
+                background:#265496;
+                padding: 1px 5px;
+                border-radius: 4px;
+                margin-left: 10px;
+                font-size: 12px;
+            }
+
+        }
+        a {
+            text-decoration: none;
+            color: #333;
+
+            &:hover {
+                color: #265496;
+            }
+        }
+        p {
+            margin-top: 7px;
+            font-size: 14px;
+            color: #999;
+        }
+    }
+`;
