ad8e9546d48db9ea76fbf89cdecc54dcb6d0f419
lucas <lucas.san.77@gmail.com>
2019-11-15 13:50:01 -0300

    listing api github users - ListGit

--- reactNative/listGit/src/pages/Main/styles.js
+++ reactNative/listGit/src/pages/Main/styles.js
@@ -12,7 +12,7 @@ export const Form = styled.View`
     border-color: #eee;
 `;
 export const Input = styled.TextInput.attrs({
-    placeholderTextColor: '#7159c1',
+    placeholderTextColor: '#999',
 })`
     flex: 1;
     height: 40px;
@@ -28,4 +28,52 @@ export const SubmitButton = styled(RectButton)`
     border-radius: 4px;
     margin-left: 10px;
     padding: 0 12px;
+`;
+
+export const List = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+})`
+    margin-top: 20px;
+`;
+
+export const User = styled.View`
+    align-items: center;
+    margin: 0 20px 30px;
+`;
+export const Avatar = styled.Image`
+    width: 64px;
+    height: 64px;
+    border-radius: 32px;
+    background: #eee;
+`;
+export const Name = styled.Text`
+    font-size: 14px;
+    color: #eee;
+    font-weight: bold;
+    margin-top: 5px;
+    text-align: center;
+`;
+export const Bio = styled.Text.attrs({
+    numberOfLines: 2,
+})`
+    font-size: 13px;
+    line-height: 18px;
+    color: #999;
+    margin-top: 5px;
+    text-align: center;
+`;
+export const ProfileButton = styled(RectButton)`
+    margin-top: 10px;
+    align-self: stretch;
+    border-radius: 4px;
+    background: #7159c1;
+    justify-content: center;
+    align-items: center;
+    height: 36px;
+`;
+export const ProfileButtonText = styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
 `;
