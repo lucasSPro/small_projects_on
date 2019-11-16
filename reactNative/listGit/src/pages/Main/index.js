02f6a9b30da18c591e3751273555a5f832ae80b4
lucas <lucas.san.77@gmail.com>
2019-11-16 00:38:35 -0300

    add list Star - ListGit

--- reactNative/listGit/src/pages/Main/index.js
+++ reactNative/listGit/src/pages/Main/index.js
@@ -1,4 +1,8 @@
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/static-property-placement */
+/* eslint-disable react/sort-comp */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Keyboard, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -19,6 +23,16 @@ import {
 } from './styles';
 
 export default class Main extends Component {
+    static navigationOptions = {
+        title: 'Usuários',
+    };
+
+    static propTypes = {
+        navigation: PropTypes.shape({
+            navigate: PropTypes.func,
+        }).isRequired,
+    };
+
     state = {
         newUser: '',
         users: [],
@@ -62,6 +76,11 @@ export default class Main extends Component {
         Keyboard.dismiss();
     };
 
+    handleNavigate = user => {
+        const { navigation } = this.props;
+        navigation.navigate('User', { user });
+    };
+
     render() {
         const { users, newUser, loading } = this.state;
         return (
@@ -96,7 +115,9 @@ export default class Main extends Component {
                             <Name>{item.name}</Name>
                             <Bio>{item.bio}</Bio>
 
-                            <ProfileButton onPress={() => {}}>
+                            <ProfileButton
+                                onPress={() => this.handleNavigate(item)}
+                            >
                                 <ProfileButtonText>
                                     {' '}
                                     Ver perfil{' '}
@@ -109,7 +130,3 @@ export default class Main extends Component {
         );
     }
 }
-
-Main.navigationOptions = {
-    title: 'Usuários',
-};
